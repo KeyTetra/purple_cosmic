24a048065491167df95dbc264eecdcbd524f592e
KeyTetra <keymtetra@gmail.com>
2025-09-17 21:49:25 -0500

    host settings

--- main.py
+++ main.py
@@ -286,5 +286,7 @@ def product_maker():
         images.update_one({"_id": ObjectId(pp)}, {"$set": {"status": 3}})
         return redirect(url_for("product_maker"))
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
